d982679b589e575ea591cbd78fdca1c96aecdd8c
kenton <38125586+Kenton1989@users.noreply.github.com>
2020-11-05 19:59:15 +0800

    Change requirement for key item.

--- cx2002grp2/stars/data/dataitem/SingleStringKeyItem.java
+++ cx2002grp2/stars/data/dataitem/SingleStringKeyItem.java
@@ -11,8 +11,8 @@ public interface SingleStringKeyItem {
     /**
      * The method used to change the key value of an item.
      * <p>
-     * This method doesn't need to do any check about database integrity like
-     * whether the new key is a existing key.
+     * The concrete method should check whether the new key is an existing key in
+     * the database before changing the key to a new one.
      * <p>
      * This method is supposed to be call by database class object only. All the
      * other objects should use the changeKey methods in the databse class to change
