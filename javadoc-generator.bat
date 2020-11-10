976ad7bf9bdb3b19bf88af4ade0e89f62294cd2f
kenton <38125586+Kenton1989@users.noreply.github.com>
2020-11-10 14:44:59 +0800

    Refine javadoc generator script

--- javadoc-generator.bat
+++ javadoc-generator.bat
@@ -1,1 +1,5 @@
-javadoc -d ../doc-html -author -private -noqualifier all -version -cp ".;.\javax\mail.jar;" cx2002grp2.stars cx2002grp2.stars.util cx2002grp2.stars.data cx2002grp2.stars.functions cx2002grp2.stars.data.dataitem cx2002grp2.stars.data.database cx2002grp2.stars.data.converter
+@echo off
+javadoc -d ../doc-html -author -noqualifier all-version -cp ".;.\javax\mail.jar;" cx2002grp2.stars cx2002grp2.stars.util cx2002grp2.stars.data cx2002grp2.stars.functions cx2002grp2.stars.data.dataitem cx2002grp2.stars.data.database cx2002grp2.stars.data.converter
+echo ==============Generation Finished==============
+echo Checkout ../doc-html for generated documentation
+pause
