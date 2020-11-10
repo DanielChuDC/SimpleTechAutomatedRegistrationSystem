07eac29c04fadb7f68fc5e4baf7f4dc42ad7894f
kenton <38125586+Kenton1989@users.noreply.github.com>
2020-11-11 03:54:46 +0800

    Move copied sending email codes.

--- cx2002grp2/stars/util/EmailNotificationSender.java
+++ cx2002grp2/stars/util/EmailNotificationSender.java
@@ -17,36 +17,43 @@ public class EmailNotificationSender implements NotificationSender {
     @Override
     public void sendNotification(User targetUser, String msg) {
         throw new UnsupportedOperationException();
-        // final String username = " "; // to be added
-        // final String password = " "; // to be added
-
-        // Properties props = new Properties();
-        // props.put("mail.smtp.auth", "true");
-        // props.put("mail.smtp.starttls.enable", "true");
-        // props.put("mail.smtp.host", "smtp.gmail.com");
-        // props.put("mail.smtp.port", "587");
-
-        // Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-        //     protected PasswordAuthentication getPasswordAuthentication() {
-        //         return new PasswordAuthentication(username, password);
-        //     }
-        // });
-
-        // try {
-
-        //     Message message = new MimeMessage(session);
-        //     message.setFrom(new InternetAddress("from-email@gmail.com"));
-        //     message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(" ")); // to be added an email addr
-        //     message.setSubject("Testing Subject");
-        //     message.setText("Dear Mail Crawler," + "\n\n No spam to my email, please!");
-
-        //     Transport.send(message);
-
-        //     System.out.println("Done");
-
-        // } catch (MessagingException e) {
-        //     throw new RuntimeException(e);
-        // }
     }
 
+    /**
+     * Send a email from given sender account to the given receiver containing the
+     * given subject and content.
+     * 
+     * @param senderEmail    the sender's email account.
+     * @param senderPassword the sender's password.
+     * @param receiverEmail  the receiver's email account.
+     * @param subject        the subject of email.
+     * @param content        the content of email.
+     */
+    private void sendEmail(String senderEmail, String senderPassword, String receiverEmail, String subject, String content) {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(senderEmail, senderPassword);
+            }
+        });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(senderEmail));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(receiverEmail));
+            message.setSubject(subject);
+            message.setText(content);
+
+            Transport.send(message);
+            
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
