07e910e5b07937f870d5754c945135143e076af2
kenton <38125586+Kenton1989@users.noreply.github.com>
2020-11-11 03:55:50 +0800

    Update Authenticator.java

--- cx2002grp2/stars/Authenticator.java
+++ cx2002grp2/stars/Authenticator.java
@@ -1,5 +1,6 @@
 package cx2002grp2.stars;
 
+import java.io.Console;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -8,17 +9,41 @@ import java.util.Base64;
 import java.util.List;
 
 import cx2002grp2.stars.data.dataitem.User;
+import cx2002grp2.stars.functions.AbstractFunction;
 import cx2002grp2.stars.functions.Function;
 
 /**
  * 
  */
 public class Authenticator {
+    private static Authenticator instance = new Authenticator();
+
+    public static Authenticator getInstance() {
+        return instance;
+    }
+
+    private MessageDigest digester;
+
+    private Authenticator() {
+        // TODO - Add initialization code if necessary.
+
+        // Get SHA-256 Hasher
+        try {
+            digester = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     /**
      * 
      * @return
      */
     public User login() {
+        // TODO - implement method
+        
+        User.Domain[] allDomain = User.Domain.values();
+        
         return null;
     }
 
@@ -28,6 +53,10 @@ public class Authenticator {
      * @return
      */
     public User login(User.Domain useDomain) {
+        // TODO - implement method
+
+        Console console = System.console();
+
         return null;
     }
 
@@ -37,20 +66,31 @@ public class Authenticator {
      * @return
      */
     public List<Function> accessibleFunctions(User user) {
-        return new ArrayList<>();
-    }
+        // TODO - implement method
+        List<Function> result = new ArrayList<>();
 
-    private String hashPasswd(String passwd) {
-        // Get SHA-256 Hasher
-        MessageDigest digester;
-        try {
-            digester = MessageDigest.getInstance("SHA-256");
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
+        for (Function func: AbstractFunction.allFunctions()) {
+
         }
 
-        // Convert the string to bytes, and do the hashing
-        byte[] bytesToHash = passwd.getBytes(StandardCharsets.UTF_8);
+        return result;
+    }
+
+    /**
+     * Hashing function used to hash a password.
+     * <p>
+     * SHA-256 is used to hash the password and the result is encoded into Base64
+     * string.
+     * 
+     * @param password the password to be hashed.
+     * @return
+     */
+    private String hashPasswprd(char[] password) {
+
+        // Convert the string into bytes
+        byte[] bytesToHash = String.valueOf(password).getBytes(StandardCharsets.UTF_8);
+
+        // Do hashing
         byte[] hashedBytes = digester.digest(bytesToHash);
 
         // Encoding the bytes with Base64 for textual storage
