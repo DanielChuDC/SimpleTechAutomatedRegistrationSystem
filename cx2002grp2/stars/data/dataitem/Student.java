4135a6c96f44733621ab61c9b3ec217c88c23851
kenton <38125586+Kenton1989@users.noreply.github.com>
2020-11-13 19:55:00 +0800

    Fix syntax error

--- cx2002grp2/stars/data/dataitem/Student.java
+++ cx2002grp2/stars/data/dataitem/Student.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import cx2002grp2.stars.data.Gender;
+import cx2002grp2.stars.data.dataitem.Registration.Status;
 
 /**
  * A class saving information of a student.
