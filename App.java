0d808307f02cf7a45b7d4a66b5fd0d5fe8bf67b3
Kenton <38125586+Kenton1989@users.noreply.github.com>
2020-11-17 02:12:46 +0800

    Refine email sender to support async

--- App.java
+++ App.java
@@ -5,6 +5,6 @@ public class App {
         MainApp app = MainApp.getApp();
         app.initialize(args);
         app.run();
-        System.out.println("Program end");
+        System.out.println("Waiting for email sender...");
     }
 }
