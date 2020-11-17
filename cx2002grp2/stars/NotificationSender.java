d8609408175f061cfc8b7ef25305898d65499325
Kenton <38125586+Kenton1989@users.noreply.github.com>
2020-11-17 12:28:53 +0800

    Refine notification sender

--- cx2002grp2/stars/NotificationSender.java
+++ cx2002grp2/stars/NotificationSender.java
@@ -8,8 +8,15 @@ public interface NotificationSender {
      * Send the given text message to the given user.
      * 
      * @param targetUser the target user to send the notification.
+     * @param subject    the subject of notification.
      * @param msg        the notification to be sent
      */
     void sendNotification(User targetUser, String subject, String msg);
+
+    /**
+     * Send an registration approval message for the given registration.
+     * 
+     * @param reg the registration being approved.
+     */
     void sendWaitlistNotification(Registration reg);
 }
