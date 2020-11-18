1a75df97a352809f2c1f2eb6ef0a330011985d9d
kenton <38125586+Kenton1989@users.noreply.github.com>
2020-11-18 19:03:28 +0800

    Finish index deletionfunction

--- cx2002grp2/stars/functions/EditCourseIndex.java
+++ cx2002grp2/stars/functions/EditCourseIndex.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 
 import cx2002grp2.stars.database.CourseDB;
 import cx2002grp2.stars.database.CourseIndexDB;
+import cx2002grp2.stars.database.UserDB;
 import cx2002grp2.stars.dataitem.Course;
 import cx2002grp2.stars.dataitem.CourseIndex;
 import cx2002grp2.stars.dataitem.User;
@@ -54,6 +55,82 @@ public class EditCourseIndex extends AbstractFunction {
 
     @Override
     protected void implementation(User user) {
+        int funcNo = selectFunction("Create Course Index", "Edit Course Index", "Delete Course Index");
+        switch (funcNo) {
+            case 1:
+                addCourseIndex(user);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+    }
+
+    /**
+     * Interact with user for adding course index.
+     * 
+     * @param user the user of this function
+     */
+    private void addCourseIndex(User user) {
+        // Input course
+        Course course;
+        while (true) {
+            System.out.println("Enter the course code under with the new index to be added:");
+            String courseCode = sc().nextLine();
+            course = CourseDB.getDB().getByKey(courseCode);
+            if (course == null) {
+                System.out.println("Course code does not exist.");
+                if (!askYesNo("Try again?")) {
+                    return;
+                }
+            } else {
+                break;
+            }
+        }
+
+        while (true) {
+            addCourseIndex(user, course);
+            if (!askYesNo("Add another index for course " + course.getCourseCode() + "?")) {
+                return;
+            }
+        }
+    }
+
+    /**
+     * Interact with user for deleting course index.
+     * 
+     * @param user the user of this function
+     */
+    private void deleteCourseIndex(User user) {
+        CourseIndex index;
+
+        while (true) {
+            System.out.println("Enter the course index to be deleted:");
+            String indexNo = sc().nextLine().trim();
+            index = CourseIndexDB.getDB().getByKey(indexNo);
+            if (index == null) {
+                System.out.println("Course index does not exist.");
+                if (!askYesNo("Try again?")) {
+                    return;
+                }
+            } else {
+                break;
+            }
+        }
+
+        System.out.println("The index to be deleted: ");
+        tbPrinter().printIndexAndSchedule(index);
+
+        tbPrinter().printBreakLine("WARNING", '=');
+        System.out.println("Deleting an index will results in all the registrations under it being deleted.");
+        tbPrinter().printBreakLine("", '=');
+
+        if (!askYesNo("Confirm delete index "+index.getIndexNo()+"?")) {
+            System.out.println("Deletion cancelled.");
+        }
+
+        CourseIndexDB.getDB().delItem(index);
 
     }
 
@@ -62,12 +139,19 @@ public class EditCourseIndex extends AbstractFunction {
      * 
      * @param user   the user trying to run the function
      * @param course the course under which the new index is add
-     * @throws NullPointerException
+     * @return true if a new course index is added, otherwise, return false.
+     * @throws NullPointerException     if any argument is null
+     * @throws IllegalArgumentException if any argument does not exist in the
+     *                                  database
+     * @throws AccessControlException   if the user have no access to this function.
      */
-    public void addCourseIndex(User user, Course course) {
+    public boolean addCourseIndex(User user, Course course) {
         Objects.requireNonNull(user);
         Objects.requireNonNull(course);
 
+        if (!UserDB.getDB().hasItem(user)) {
+            throw new IllegalArgumentException("The input user does not exist in the database.");
+        }
         if (!CourseDB.getDB().hasItem(course)) {
             throw new IllegalArgumentException("The input course does not exist in the database.");
         }
@@ -82,18 +166,37 @@ public class EditCourseIndex extends AbstractFunction {
         while (true) {
             System.out.print("Enter new index No.: ");
             indexNo = sc().nextLine().trim();
+
             CourseIndex existingIndex = CourseIndexDB.getDB().getByKey(indexNo);
             if (existingIndex != null) {
                 System.out.println("The following index already exist: ");
                 tbPrinter().printIndexAndSchedule(existingIndex);
-                if (askYesNo("Enter another index?")) {
-                    continue;
-                } else {
-                    return;
+                if (!askYesNo("Enter another index?")) {
+                    return false;
                 }
             } else {
                 break;
             }
+
+        }
+        int maxVcc = enterInt("Enter max vacancy of course index: ", 0, Integer.MAX_VALUE);
+
+        System.out.println("The following course index will be created: ");
+        System.out.printf("Course Code: %s, Index: %s, Max Vcc: %d\n", course.getCourseCode(), indexNo, maxVcc);
+
+        if (!askYesNo("Confirm?")) {
+            System.out.println("Index creation cancelled.");
+            return false;
+        }
+
+        try {
+            CourseIndex newIndex = new CourseIndex(indexNo, course, maxVcc);
+            CourseIndexDB.getDB().addItem(newIndex);
+            return true;
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+            System.out.println("Index creation failed.");
+            return false;
         }
 
     }
